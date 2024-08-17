b249b8b9b2eb40b7932557a4acaeb47171c8c7bb
hungvn-kimei <hungvn@kimei.vn>
2024-08-17 19:08:55 +0700

    learn Cors-policy apply client side rendering CSR

--- CORS-Policy/api/server.js
+++ CORS-Policy/api/server.js
@@ -1,10 +1,19 @@
+const fs = require('fs');
+const https = require('https');
+
 const express = require('express');
 const cors = require('cors');
+const cookieParse = require('cookie-parser');
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(cookieParse());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Fake DB
@@ -36,14 +45,68 @@ const db = {
     ]
 }
 
+// Session
+const sessions = {}
+
 // [GET] /api/posts
 app.get('/api/posts', (req, res) => {
     res.json(db.posts);
-})
+});
 
-app.listen(port, () => {
+//[POST] /api/auth/login
+app.post('/api/auth/login', (req, res) => {
+    const { email, password } = req.body;
+    const user = db.users.find(user => user.email === email && user.password === password);
+    if (!user) {
+        return res.status(401)
+            .json({
+                message: 'Unauthorized'
+            });
+    }
+
+    // tạo phiên đăng nhập
+    const sessionId = Date.now().toString();
+    sessions[sessionId] = { sub: user.id };
+    // sau dòng này thì session đã được tạo ra ở phía server
+
+    //sau đó ta sẽ trả cho client cái sessionId này để client nó lưu vào cookie. lưu ý thực tế ta sẽ mã hóa cái sessionId này 
+    res.setHeader(
+        'Set-Cookie',
+        `sessionId=${sessionId}; HttpOnly; Max-Age=3600; SameSite=None; Secure; Partitioned`
+    ).json(user);
+});
+
+// [GET] /api/auth/me
+app.get('/api/auth/me', (req, res) => {
+    const session = sessions[req.cookies.sessionId];
+    if (!session) {
+        return res.status(401)
+            .json({
+                message: 'Unauthorized'
+            });
+    }
+    const user = db.users.find(user => user.id === session.sub);
+
+    if (!user) {
+        return res.status(401)
+            .json({
+                message: 'Unauthorized'
+            });
+    }
+
+    res.json(user);
+});
+
+https.createServer({
+    key: fs.readFileSync('testcookie.com+2-key.pem'),
+    cert: fs.readFileSync('testcookie.com+2.pem'),
+}, app).listen(port, () => {
     console.log(`Demo app is running on port ${port}`);
 });
+
+// app.listen(port, () => {
+//     console.log(`Demo app is running on port ${port}`);
+// });
 
 // Server side rendering (SSR)
 // Client side rendering (CSR)
--- CORS-Policy/ui/src/App.jsx
+++ CORS-Policy/ui/src/App.jsx
@@ -1,44 +1,97 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-const baseApi = 'http://localhost:3000/api';
+const baseApi = 'https://testcookie.com:3000/api';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState('');
+  const [fields, setFields] = useState({
+    email: 'nguyenvana@gmail.com',
+    password: '123456'
+  });
+
+  const setFieldsValue = ({ target: { name, value } }) => {
+    setFields(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  }
+
+  const handleLogin = e => {
+    e.preventDefault();
+    setError('');
+    fetch(`${baseApi}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify(fields)
+    })
+      .then(res => {
+        if (res.ok) return res.json();
+        throw res;
+      })
+      .then(user => {
+        console.log(user);
+      })
+      .catch((error) => {
+        if (error.status === 401) {
+          return setError('Email hoặc mật khẩu không chính xác');
+        }
+        setError('Lỗi không xác định! Liên hệ contact@abc.com để được hỗ trợ!');
+      });
+  }
 
   useEffect(() => {
-    fetch(`${baseApi}/posts`)
-    .then(res => res.json())
-    .then(posts => {
-      console.log(posts);
-    });
+    fetch(`${baseApi}/auth/me`, {
+      credentials: 'include'
+    })
+      .then(res => {
+        if (res.ok) return res.json();
+        throw res;
+      })
+      .then(me => {
+        setUser(me);
+      })
+      .catch(() => {
+      });
   })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {user ? (
+        <p>Xin chào, {user.name}</p>
+      ) : (
+        <>
+          <h1>Login</h1>
+          <form onSubmit={handleLogin}>
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={fields.email}
+              onChange={setFieldsValue}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={fields.password}
+              onChange={setFieldsValue}
+            />
+            <br />
+            <button>Login</button>
+          </form>
+          {!!error && <p style={{ color: 'red' }}>{error}</p>}
+        </>
+      )}
+    </div>
   )
 }
 
